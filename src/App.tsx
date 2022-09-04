0104e33f86e487528e37b9f5f792154f89c83158
adamdebalke <youngadam65@gmail.com>
2022-09-04 22:15:05 +0300

    Create RegisterPage & RegisterForm Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-//import { FC, PropsWithChildren, useState } from 'react';
 import React from 'react';
-import {Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { AuthenticationPage } from './pages/RegisterPage';
 
 function App() {
   return (
-  <>
-    <Routes>
-     <Route path="/" element={<div>hello test</div>}></Route>
+    <>
+      <Routes>
+        <Route path="/" element={<AuthenticationPage />}></Route>
         <Route
-           path="conversations"
-            element={
-              <div>
-                <div>conversations</div>
-                <Outlet />
-              </div>
-            }
+          path="conversations"
+          element={
+            <div>
+              <div>Conversations</div>
+              <Outlet />
+            </div>
+          }
         >
-           <Route path=":id" element={<div>Conversaiton ID Page</div>} />
+          <Route path=":id" element={<div>Conversation ID Page</div>} />
         </Route>
-     </Routes>
-   </>
+      </Routes>
+    </>
   );
 }
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-/*root.render(
+root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
@@ -17,4 +20,3 @@ const root = ReactDOM.createRoot(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-*/
